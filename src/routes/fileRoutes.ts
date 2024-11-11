085a5496c1fae8667e8398b729beb941b23f823c
Wanatszef <kacperryszardwanat@gmail.com>
2024-11-11 18:37:27 +0100

    fixed with Eslint

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -1,26 +1,25 @@
-import { Elysia } from 'elysia';
-import { handleFileUpload } from '../controllers/fileController';
-import {join} from 'path';
-import {readFileSync} from 'fs';                                                
+import { Elysia } from "elysia";
+import { handleFileUpload } from "../controllers/fileController";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 const fileRoutes = new Elysia();
 
+fileRoutes.post("/upload", handleFileUpload);
 
-fileRoutes.post('/upload', handleFileUpload);
-
-fileRoutes.get('', () => {
-    const htmlPath = join(__dirname, '../public/index.html');
-    const htmlContent = readFileSync(htmlPath, 'utf-8');
+fileRoutes.get("", () => {
+    const htmlPath = join(__dirname, "../public/index.html");
+    const htmlContent = readFileSync(htmlPath, "utf-8");
     return new Response(htmlContent, {
-        headers: { 'Content-Type': 'text/html' },
+        headers: { "Content-Type": "text/html" },
     });
 });
 
-fileRoutes.get('/script.js', () => {
-    const jsPath = join(__dirname, '../public/script.js');
-    const jsContent = readFileSync(jsPath, 'utf-8');
+fileRoutes.get("/script.js", () => {
+    const jsPath = join(__dirname, "../public/script.js");
+    const jsContent = readFileSync(jsPath, "utf-8");
     return new Response(jsContent, {
-        headers: { 'Content-Type': 'application/javascript' },
+        headers: { "Content-Type": "application/javascript" },
     });
 });
 
--- src/utils/checkDomainStatus.ts
+++ src/utils/checkDomainStatus.ts
@@ -1,7 +1,6 @@
 import findCart from "./findCart";
 import findAds from "./findAds";
-import {Domain} from "./Domain";
-import advanceCheckDomainStatus from "./advancedCheckDomainStatus";
+import { Domain } from "./Domain";
 
 async function checkDomainStatus(url: string): Promise<Domain> {
     try {
@@ -9,54 +8,53 @@ async function checkDomainStatus(url: string): Promise<Domain> {
 
         const response = await fetch(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
-            }
+                "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+            },
         });
-        
+
         const status: number = response.status;
         const pageSource: string = await response.text();
 
-
         // Sprawdzamy, czy status pozwala na analizę treści
-        switch(status) {
-            case 200: // OK
-            case 301: // Przekierowanie stałe
-            case 302: // Przekierowanie tymczasowe
-            case 304: // Nie modyfikowano
-                domain.setStatus(status);
-                domain.setCart(findCart(pageSource));
-                domain.setAdvertisement(findAds(pageSource));
-                break;
+        switch (status) {
+        case 200: // OK
+        case 301: // Przekierowanie stałe
+      case 302: // Przekierowanie tymczasowe
+        case 304: // Nie modyfikowano
+        domain.setStatus(status);
+            domain.setCart(findCart(pageSource));
+            domain.setAdvertisement(findAds(pageSource));
+        break;
 
-            case 403:
-            case 401:
-            case 429:
-            case 409:
-                domain.setStatus(status);
-                //cledomain = await advanceCheckDomainStatus(url,status);
-                break;
-            case 400: // Błędne zapytanie
-            case 404: // Nie znaleziono
-            case 500: // Błąd wewnętrzny serwera
-            case 502: // Zła brama
-            case 503: // Niedostępny serwis
-            case 504: // Brak odpowiedzi od bramy
-                domain.setStatus(status);
-                break;
+      case 403:
+        case 401:
+        case 429:
+      case 409:
+            domain.setStatus(status);
+            //cledomain = await advanceCheckDomainStatus(url,status);
+        break;
+        case 400: // Błędne zapytanie
+        case 404: // Nie znaleziono
+      case 500: // Błąd wewnętrzny serwera
+        case 502: // Zła brama
+        case 503: // Niedostępny serwis
+        case 504: // Brak odpowiedzi od bramy
+            domain.setStatus(status);
+        break;
 
-            default:
-                if (status > 308) { 
-                    domain.setStatus(status);
-                } else {
-                    domain.setCart(findCart(await response.text()));
-                }
-                break;
+        default:
+        if (status > 308) {
+                domain.setStatus(status);
+            } else {
+                domain.setCart(findCart(await response.text()));
         }
-        
-        return domain;
+            break;
+        }
 
+        return domain;
     } catch (error) {
-        console.error('Błąd podczas pobierania strony:', error);
+        console.error("Błąd podczas pobierania strony:", error);
         return new Domain(url);
     }
 }
